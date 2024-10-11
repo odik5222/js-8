d4fa1e447c54dea48ac873bd1f23a2b4b4ea2204
odik_web <116338011+odik5222@users.noreply.github.com>
2024-10-11 12:01:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let money = 10000;
+
+let account = 7777;
+
+const name = prompt('как тебя зовут?').toLowerCase();
+
+switch (name) {
+    case "alex":
+        const account = prompt('Аккаунт');
+        
+    case "7777":
+        const money = prompt('сколько обноличать?');
+        
+    case '10000':
+        const cash = alert('отлично')
+        
+    default:
+        alert('аккаунт не найден');
+}
